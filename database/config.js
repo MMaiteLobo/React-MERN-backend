d454398053a01d1617e402ac44883f5cae664d35
MMaiteLobo <maitemlobo@gmail.com>
2025-06-13 16:45:27 -0300

    correcciones2

--- database/config.js
+++ database/config.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN);
+        console.log('Intentando conectar a la base de datos...');
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('DB Online');
     } catch (error) {
-        console.log(error);
+        console.error('Error al conectar a la base de datos:', error);
         throw new Error('Error al iniciar la base de datos');
     }
 }
